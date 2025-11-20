45416711225a64dc7150b53958b4b981004ac826
teja-daws-ai <chteja717@gmail.com>
2025-11-20 15:47:09 +0530

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1:: Hi $PERSON2, How are you"
+echo "$PERSON2:: Hi $PERSON1, I'm good. How are you..."
+echo "$PERSON1:: I'm doing good $PERSON2, whats going on"
+echo "$PERSON2:: I started learning shell scripting $PERSON1"
