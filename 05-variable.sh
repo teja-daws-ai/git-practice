770a9c22397d03b5aa4acb9db5cb47af38d6e7a1
teja-daws-ai <chteja717@gmail.com>
2025-11-20 16:02:49 +0530

    read variable

---
+++ 05-variable.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Please enter your username::"
+
+read -s USERNAME #takes input into USERNAME variable 
+
+echo "Username entered is: $USERNAME"
+
+echo "Please enter your password::"
+
+read -s PASSWORD #takes input into PASSWORD variable 
+
+echo "Password entered is: $PASSWORD"
