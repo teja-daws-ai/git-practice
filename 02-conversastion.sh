d7bf8bd6fea5af2ecdf6ba40ed4a5664ad963cc8
teja-daws-ai <chteja717@gmail.com>
2025-11-20 15:41:29 +0530

    added conversation script

---
+++ 02-conversastion.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Ramesh:: Hi Suresh, How are you"
+echo "Suresh:: Hi Ramesh, I'm good. How are you..."
+echo "Ramesh:: I'm doing good suresh, whats going on"
+echo "Suresh:: I started learning shell scripting Ramesh"
