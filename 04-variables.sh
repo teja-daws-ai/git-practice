4fda958a40c03a1b26e61794c8f85d659bef3517
teja-daws-ai <chteja717@gmail.com>
2025-11-20 17:25:32 +0530

    read variable

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#runtime variable script/if we pass runtime aruguments script will work
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hi $PERSON2, How are you"
+echo "$PERSON2:: Hi $PERSON1, I'm good. How are you..."
+echo "$PERSON1:: I'm doing good $PERSON2, whats going on"
+echo "$PERSON2:: I started learning shell scripting $PERSON1"
