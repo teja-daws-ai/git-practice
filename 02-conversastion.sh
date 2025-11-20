f04187aa3a1ddc6694344cd43876aadc09265e37
teja-daws-ai <chteja717@gmail.com>
2025-11-20 16:31:36 +0530

    read variable

--- 02-conversastion.sh
+++ 02-conversastion.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Ramesh:: Hi Suresh, How are you"
 echo "Suresh:: Hi Ramesh, I'm good. How are you..."
-echo "Ramesh:: I'm doing good suresh, whats going on"
+echo "Ramesh:: I'm doing good Suresh, whats going on"
 echo "Suresh:: I started learning shell scripting Ramesh"
